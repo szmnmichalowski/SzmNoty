d28cea10f740d58c2b61c72a97ac858b8310ef0b
Szymon Michalowski <szmnmichalowski@gmail.com>
2017-03-14 20:03:42 +0100

    Add tests for module file

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -10,4 +10,17 @@ class ModuleTest extends TestCase
      * @var Module
      */
     protected $module;
+
+    public function setUp()
+    {
+        $this->module = new Module();
+    }
+
+    /**
+     * @covers SzmNoty\Module::getConfig
+     */
+    public function testGetConfig()
+    {
+        $this->assertTrue(is_array($this->module->getConfig()));
+    }
 }
